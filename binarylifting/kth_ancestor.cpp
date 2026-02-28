a792385ec3ffd7f71cf1f057d32eeccdcc43e18b
Radesh07 <radeshannam@gmail.com>
2026-03-01 03:25:35 +0530

    progress

---
+++ binarylifting/kth_ancestor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> parent(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> parent[i];
+    }
+    const int LOG = 20;
+    vector<vector<int>> table(LOG, vector<int>(n + 1));
+    for (int i = 1; i <= n; i++) {
+        table[0][i] = parent[i];
+    }
+    for (int i = 1; i < LOG; i++) {
+        for (int j = 1; j <= n; j++) {
+            table[i][j] = table[i - 1][ table[i - 1][j] ];
+        }
+    }
+    int q;
+    cin >> q;
+    while (q--) {
+        int a, k;
+        cin >> a >> k;
+        for (int i = 0; i < LOG; i++) {
+            if (k & (1 << i)) {
+                a = table[i][a];
+                if (a == 0) break;
+            }
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
---
+++ graphs_cses/Planets_Queries_I.cpp
@@ -0,0 +1,2 @@
+// use binary lifing , since it is a funcitonal graph 
+// the  equation of this question , --> a number can be broken down to the powers of two 
