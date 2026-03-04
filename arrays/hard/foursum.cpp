c546a787b414615d5d8d44299c018293c58de052
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2026-03-04 05:46:14 +0530

    onwards

--- arrays/hard/foursum.cpp
+++ arrays/hard/foursum.cpp
@@ -1,3 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cerr << "[" << #x << "] = " << (x) << '\n';
+#define int long long
+#define all(x) x.begin(), x.end()
+#define f(i, n) for (int i = 0; i < n; i++)
+#define r(dt, name) for (auto &dt : name)
+#define debvar(x) cerr << #x << " = " << (x) << " | ";
+#define debline cerr << "Line: " << __LINE__ << "\n";
+#define check(msg) cerr << "check: " << msg << " at line: " << __LINE__ << '\n';
+
     vector<vector<int>> fourSum(vector<int>& vec, int tot) {
         int n = vec.size();
          set<vector<int>> st;
@@ -33,3 +45,17 @@
           }
           return ans;
     }
+void fastInputOutput() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int32_t main() {
+    fastInputOutput();
+    int T = 1;
+    cin >> T;
+    while(T--) {
+
+    }
+    return 0;
+}
